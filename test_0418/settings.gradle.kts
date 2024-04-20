14af21255a7c2961fa5fe665ed8019af6b2e7c24
Laiii151 <eve021282@gmail.com>
2024-04-20 17:49:26 +0800

    113-04-20-Test

---
+++ test_0418/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TicketPurchase_0418"
+include(":app")
+ 
